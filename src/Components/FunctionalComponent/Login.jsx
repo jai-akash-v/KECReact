7c4b41e51fbda0cffbef502e2324c4ea07902436
jaiakash95 <jaiakashv.22aim@kongu.edu>
2025-01-06 08:51:54 +0530

    Day 8

--- src/Components/FunctionalComponent/Login.jsx
+++ src/Components/FunctionalComponent/Login.jsx
@@ -1,16 +1,60 @@
 //import React from "react";
+import "../../assets/css/NavBar.css";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
 var Login = () => {
+  var navigate = useNavigate();
+  var [email, SetEmail] = useState("");
+  var [password, SetPassword] = useState("");
+  var login = async (event) => {
+    event.preventDefault();
+    var req = axios
+      .post("http://localhost:3000/login", {
+        email: email,
+        password: password,
+      })
+      .then((res) => {
+        console.log(res.data.message)
+        var isLoggedIn = res.data.isLoggedIn;
+        console.log(isLoggedIn)
+        if(isLoggedIn){
+            navigate("/home");
+      }
+  })
+    .catch((err) => console.log(err));
+    console.log(req);
+  };
+
+  var navi = () =>{
+    navigate("/signup");
+  }
+
   return (
-    <div>
-      <form style={{padding:10}} >
-      <h1>Login</h1>
-        <label>Name: </label>
-        <input type='text' id='Name' placeholder='Enter your name' />
+    <div style={{padding:5}} className='form-box' onSubmit={login}>
+        <h1>Welcome Back!</h1>
+        <h1>Login</h1>
+      <form onSubmit={login}>
+        <label htmlFor='email'>Email: </label>
+        <input
+          type='text'
+          id='email'
+          onChange={(e) => SetEmail(e.target.value)}
+        />
         <br />
-        <label>Password: </label>
-        <input type="password" id="passwd" placeholder="Enter your password" />
+        <label htmlFor='pass'>Password: </label>
+        <input
+          type='password'
+          id='pass'
+          onChange={(e) => SetPassword(e.target.value)}
+        />
         <br />
-        <input type='submit' value='Login' />
+        <button type='submit' value='Submit'>
+          Login
+        </button>
+        <br />
+        <button type="button" onClick={navi}>No account? Sign up</button>
       </form>
     </div>
   );
--- src/Components/FunctionalComponent/Signup.jsx
+++ src/Components/FunctionalComponent/Signup.jsx
@@ -1,34 +1,74 @@
 //import React from "react";
-var Signup = () => {
-    return (
-      <div>
-        <form style={{padding:10}} onSubmit={Signup} >
-            <h1>Sign Up</h1>
-          <label>Name: </label>
-          <input type='text' id='Name' placeholder='Enter your name' />
-          <br />
-          <label>Gender:</label>
-          <br />
-          <input type='radio' name='Gender' value='Male' />
-          Male
-          <input type='radio' name='Gender' value='Female' />
-          Female
-          <br />
-          <label>Password: </label>
-        <input type="password" id="passwd" placeholder="Enter your password" />
-          <br />
-          <label>Department: </label>
-          <select id='Department' name='Department'>
-            <option value='AIML'>AIML</option>
-            <option value='AIDS'>AIDS</option>
-            <option value='CSE'>CSE</option>
-          </select>
-          <br />
-          <br />
-          <button type='submit' value='Sign up'>Sign up</button>
-        </form>
-      </div>
-    );
+import "../../assets/css/NavBar.css";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+var SignUp = () => {
+    var navigate = useNavigate();
+  const [FName, SetFName] = useState("");
+  const [LName, SetLName] = useState("");
+  const [password, SetPassword] = useState("");
+  const [email, SetEmail] = useState("");
+
+  var signup = async (event) => {
+    event.preventDefault();
+    var req = axios.post("http://localhost:3000/signup", {
+      firstName: FName,
+      lastName: LName,
+      email: email,
+      password: password
+    });
+    console.log(req);
+    navigate('/login');
   };
-  export default Signup;
-  
+  return (
+    <div className='form-box'>
+      <form style={{padding:5}} onSubmit={signup}>
+      <h1>Sign Up</h1>
+        <label htmlFor='Fname'>First Name: </label>
+        <input
+          type='text'
+          id='FName'
+          placeholder='Enter your First name'
+          onChange={(e) => {
+            SetFName(e.target.value);
+          }}
+        />
+        <br />
+        <label htmlFor='Lname'>Last Name: </label>
+        <input
+          type='text'
+          id='LName'
+          placeholder='Enter your Last name'
+          onChange={(e) => {
+            SetLName(e.target.value);
+          }}
+        />
+        <br />
+        <label htmlFor='email'>Email: </label>
+        <input
+          type='email'
+          id='email'
+          placeholder='Enter your Email'
+          onChange={(e) => {
+            SetEmail(e.target.value);
+          }}
+        />
+        <br />
+        <label htmlFor='pass'>Password: </label>
+        <input
+          type='password'
+          id='pass'
+          placeholder='Enter your password'
+          onChange={(e) => {
+            SetPassword(e.target.value);
+          }}
+        />
+        <br />
+        <button type='submit'>Sign Up </button>
+      </form>
+    </div>
+  );
+};
+export default SignUp;
